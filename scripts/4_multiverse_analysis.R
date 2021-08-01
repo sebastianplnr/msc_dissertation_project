ce1b49e07e95e2e92fb884a9651c2505946dc0c2
sebastianplnr <sebastian.ploner@gmail.com>
2021-08-01 09:46:33 +0200

    Adding comments to multiverse script

--- scripts/4_multiverse_analysis.R
+++ scripts/4_multiverse_analysis.R
@@ -34,7 +34,7 @@ if(!is.null(sessionInfo()$otherPkgs)) {
 
 
 # Download and install missing packages
-requiredPackages = c("psych", "plotrix", "rstudioapi", "DescTools", # packages needed?
+requiredPackages = c(# "psych", "plotrix", "rstudioapi", "DescTools", # packages needed?
                      "here", "data.table", "tidyverse", "lme4", "parallel", "PupillometryR")
 
 missingPackages = requiredPackages[!requiredPackages %in% installed.packages()[ , "Package"]]
@@ -66,25 +66,25 @@ dat = data.frame(fread(here::here("data", "3_prepared_data.csv")))
 
 #...# Prepare variables needed for specification curve analysis
 # Full variable names
-variables = c("All red cards",
-              "Skin tone (num)", "Implicit bias", "Explicit bias",
-              "Player position", "Player’s height (in cm)", "Player’s weight (in kg)", "Player’s league country", "Player’s age (in years)", "Goals scored by player", "Player’s club", "Referee’s country", "Referee", "Player’s number of victories", "Number of cards received by player", "Player", "Number of cards awarded by referee", "Number of draws", "Number games")
+variables = c("All red cards", # dv
+              "Skin tone (num)", "Implicit bias", "Explicit bias", # base var
+              "Player position", "Player’s height (in cm)", "Player’s weight (in kg)", "Player’s league country", "Player’s age (in years)", "Goals scored by player", "Player’s club", "Referee’s country", "Referee", "Player’s number of victories", "Number of cards received by player", "Player", "Number of cards awarded by referee", "Number of draws", "Number games") # covar
 
 
-# Dependent variables
+# Dependent variable
 dv = c("all_reds")
 
 
-# "Base variable"
+# Base variables
 base_var = c("skin_tone_num", "imp_bias", "exp_bias")
 
 
-# Independent variables
-co_var = c("as.factor(specific_pos)", "height_cm", "weight_kg", "as.factor(league_country)", "age_yrs", "goals", "as.factor(club)", "as.factor(ref_country)", "(1|ref)", "victories", "player_cards_received", "(1|player)", "ref_cards_assigned", "ties", "games")
+# Covariates
+covar = c("as.factor(specific_pos)", "height_cm", "weight_kg", "as.factor(league_country)", "age_yrs", "goals", "as.factor(club)", "as.factor(ref_country)", "(1|ref)", "victories", "player_cards_received", "(1|player)", "ref_cards_assigned", "ties", "games")
 
 
-# All variables needed to conduct specification curve analysis
-var_all = c(dv, base_var, co_var)
+# All variables needed to conduct multiverse analysis
+var_all = c(dv, base_var, covar)
 
 
 # create list including variables
@@ -92,18 +92,18 @@ varList = list()
 varList[["variables"]] = variables
 varList[["dv"]] = dv
 varList[["base_var"]] = base_var
-varList[["co_var"]] = co_var
+varList[["covar"]] = covar
 varList[["var_all"]] = var_all
 
 
 
 #.................. Prepare specifications: Create data frame from all combinations of covariates .........#
 # Create TRUE/FALSE matrix for every combination of covariates
-specifications = data.frame(expand.grid(rep(list(0:1), length(co_var))) == TRUE)
+specifications = data.frame(expand.grid(rep(list(0:1), length(covar))) == TRUE)
 
 
 # Set column names
-colnames(specifications) = co_var
+colnames(specifications) = covar
 
 
 # Add dependent variable and "base variable"
@@ -112,7 +112,7 @@ skin_tone_num = c(rep(TRUE, nrow(specifications)))
 imp_bias = c(rep(TRUE, nrow(specifications)))
 exp_bias = c(rep(TRUE, nrow(specifications)))
 
-specifications = add_column(specifications, all_reds, .before = co_var[1])
+specifications = add_column(specifications, all_reds, .before = covar[1])
 specifications = add_column(specifications, skin_tone_num, .after = "all_reds") # ".after" to make sure the previous column addition worked
 specifications = add_column(specifications, imp_bias, .after = "skin_tone_num")
 specifications = add_column(specifications, exp_bias, .after = "imp_bias")
@@ -125,7 +125,7 @@ base_model = paste(dv[1], " ~ ", base_var[1], "*", base_var[2], " + ", base_var[
 # Create a list including a formula for every covariate combination
 all_models = apply(specifications[ , (length(dv) + length(base_var) + 1):ncol(specifications)], # starting from first covariate as the first columns are dependent and base variables
                    1, # loop through rows
-                   function(x) paste(c(base_model, co_var[x]), collapse = " + ")) # create formulas
+                   function(x) paste(c(base_model, covar[x]), collapse = " + ")) # create formulas
 
 
 # Adding the formulas are a dedicated columns to the specification data frame
@@ -193,8 +193,8 @@ names(all_models_glm_results) = ef_sample
 
 
 # Converting list to data frame while retaining item names and combining to one data frame
-statistics_glmer = as.data.frame(do.call(rbind, all_models_glmer_results))
-statistics_glm = as.data.frame(do.call(rbind, all_models_glm_results))
+statistics_glmer = data.frame(do.call(rbind, all_models_glmer_results))
+statistics_glm = data.frame(do.call(rbind, all_models_glm_results))
 
 df = rbind(statistics_glmer, statistics_glm)
 
@@ -230,16 +230,12 @@ vibration_of_effect = df %>%
   
   ggplot(aes(x = "", y = estimate_oddsratio)) +
   
-  geom_flat_violin(aes(fill = ""),
-                   trim = FALSE,
-                   colour = "dark grey", 
-                   fill = "dark grey") +
+  geom_flat_violin(aes(fill = ""), trim = FALSE, colour = "dark grey", fill = "dark grey") +
   
   geom_point(aes(x = 0.6, y = estimate_oddsratio, colour = below_alpha),
              position = position_jitter(width = .03, seed = 123), size = 3, shape = 20, alpha = 0.6, stroke = 0) +
   
-  geom_boxplot(aes(x = 0.81, y = estimate_oddsratio),
-               alpha = 0.5, width = 0.1, colour = "black") +
+  geom_boxplot(aes(x = 0.81, y = estimate_oddsratio), alpha = 0.5, width = 0.1, colour = "black") +
   
   geom_hline(yintercept = 1.31, linetype = "dashed", color = "black") +
   
@@ -249,8 +245,7 @@ vibration_of_effect = df %>%
   
   scale_y_continuous(name = "Odds ratio", breaks = c(seq(1.0, 1.5, 0.05)), limits = c(1, 1.5), expand = c(0, 0)) +
   
-  labs(title = "Vibration of effect due to covariate specification",
-       subtitle = "Odds ratio (OR), n = 200") +
+  labs(title = "Vibration of effect due to covariate specification", subtitle = "Odds ratio (OR), n = 200") +
   
   coord_flip() + theme_classic() +
   
@@ -277,23 +272,26 @@ vibration_of_effect
 ggsave(here::here("figures", "vibration_of_effect.png"), vibration_of_effect, width = 11, height = 5)
 
 
-########
+#............................................# Covariates effects #.........................................#
 
+# Build data frame that includes the specifications and the analysis results
 analysed_specifications = inner_join(specifications, df, by = "formula")
 analysed_specifications = analysed_specifications %>% filter(estimate_oddsratio < 2 & estimate_oddsratio <= 1.5) # Excluding the zero cases makes a big difference: excluded R^2 = 0.9734, included R^2 = 0.3852
-analysed_specifications = analysed_specifications[, c(2:19, 28)]
+analysed_specifications = analysed_specifications[ , c(2:19, 28)] # select variable of interest i.e., covariates and dv
 
 colnames(analysed_specifications) = c("skin_tone_num", "imp_bias", "exp_bias", "specific_pos", "height_cm", "weight_kg", "league_country", "age_yrs", "goals", "club", "ref_country", "ref", "victories", "player_cards_received", "player", "ref_cards_assigned", "ties", "games", "estimate_oddsratio")
 
+# Run linear model to get the specified effects of each covariate (included yes/no)
 impact_mod = lm(estimate_oddsratio ~ factor(specific_pos) + factor(height_cm) + factor(weight_kg) + factor(league_country) + factor(age_yrs) + factor(goals) + factor(club) + factor(ref_country) + factor(ref) + factor(victories) + factor(player_cards_received) + factor(player) + factor(ref_cards_assigned) + factor(ties) + factor(games),
                 data = analysed_specifications)
 impact_summary = summary(impact_mod)
 
+# Extract data from model and save as data frame
 impact_coef = data.frame(impact_summary$coefficients)[c(-1), 1]
 impact_se = data.frame(impact_summary$coefficients)[c(-1), 2]
 impact_pvalue = data.frame(impact_summary$coefficients)[c(-1), 4]
-
 impact_names = c("Position", "Height", "Weight", "Lg. country", "Age", "Goals", "Club", "Ref country", "Ref", "Victories", "Cards rec.", "Player", "Cards assig.", "Ties", "Games")
+
 impact_df = data.frame(cbind(impact_names,
                              impact_coef,
                              impact_se,
@@ -308,7 +306,7 @@ impact_df$ci_upper_oddsratio = with(impact_df, exp(ci_upper))
 
 impact_df$below_alpha = with(impact_df, ifelse(as.numeric(impact_pvalue) < 0.05, "Significant", "Non-significant"))
 
-
+# Visualise the specified effect of covariates
 covariate_effects = impact_df %>%
   
   ggplot(aes(x = reorder(x = impact_names, X = estimate_oddsratio), estimate_oddsratio)) +
@@ -323,13 +321,12 @@ covariate_effects = impact_df %>%
   
   scale_y_continuous(name = "Odds ratio", breaks = c(seq(0.85, 1.1, 0.05)), limits = c(0.85, 1.1)) +
   
-  labs(title = "Specified effect of covatiates",
-       subtitle = "Odds ratio (OR), n = 200",
-       x = "Covariate") +
+  labs(title = "Specified effect of covatiates", subtitle = "Odds ratio (OR), n = 200", x = "Covariate") +
   
   theme_classic() +
   
   theme(panel.grid.major.y = element_line(colour = "grey"),
+        axis.text.x = element_text(angle = 45, hjust = 1),
         plot.margin = unit(c(0.5, 1, 0.5, 1), "cm"),
         legend.position = "top",
         legend.justification = "left",
